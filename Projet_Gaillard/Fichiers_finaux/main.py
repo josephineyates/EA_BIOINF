63d365a317b4a61f48124462135b72f7b97a6695
Pierre-Emmanuel POULET <pierre-emmanuel.poulet@polytechnique.edu>
2019-03-17 18:09:15 +0100

    Updated main

--- Projet_Gaillard/Fichiers_finaux/main.py
+++ Projet_Gaillard/Fichiers_finaux/main.py
@@ -5,18 +5,23 @@ import methods as mt
 def parse_args():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-align', default='NW_N', type=str, help="name of NW type to run, either 'NW_N' for multiple comparison or 'NW_struc' for structure information")
-    parser.add_argument('-namefile', default='BBS11001.fasta', type=str, help="fasta file name to study alignments")
+    parser.add_argument('-namefile', default='BBS11001.fasta', type=str, help="fasta file name to study alignments, select 'all' to get a global score compared to ClustalW")
     args = parser.parse_args()
     return args
 
 def main():
     args = parse_args()
-    sequences = mt.ouvertureFASTA("balibase/RV11.unaligned/"+args.namefile)
     #TODO : ne pas oublier de rajouter les ID pour pouvoir enregistrer en FASTA
-    alignments=mt.alignNseq(sequences,args.align)
-    #TODO écrire save_align_FASTA qui sauve les alignments en FASTA
-    #save_align_FASTA(name_test_file,alignments)
-    mt.bali_score("balibase/RV11.unaligned/"+args.namefile,name_test_file)     
+    if args.align == "NW_N":
+        if args.namefile == "all":
+            mt.eval_clustalw("score")
+        else:
+            sp_, tc_ = mt.align_score(args.namefile, algo="NW_blosum", silent=False)
+    elif args.align == "NW_struct":
+        if args.namefile == "all":
+            mt.eval_clustalw("score_NW_struct")
+        else:
+            sp_, tc_ = mt.align_score(args.namefile, algo="NW_structural", silent=False)  
 
 
 if __name__ == "__main__":
